c713d60a9976c8a2105ce0fd708db6b7beed2437
xschen <xs0040@gmail.com>
2017-11-13 16:59:18 +0800

    source setup

---
+++ src/main/java/com/github/chen004/eswrapper/models/IndexProperty.java
@@ -0,0 +1,16 @@
+package com.github.chen004.eswrapper.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+/**
+ * Created by xschen on 16/7/2017.
+ */
+@Getter
+@Setter
+public class IndexProperty {
+   private String name;
+   private String type;
+}
